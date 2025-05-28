5a67bb0495e18709fe82872ab6d6af0a3efd865e
David García <d.garcia@colegioarula.com>
2025-05-28 14:52:54 +0200

    cambio rutas

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-// js/jobs.js
 // js/main.js
-import { renderJobs } from "/jobs.js";
-import { renderTips } from "/tips.js";
-import { handleForm } from "/form.js";
+import { renderJobs } from "./jobs.js";
+import { renderTips } from "./tips.js";
+import { handleForm } from "./form.js";
 
 // Ejecutar funciones al cargar la página
 renderJobs();
